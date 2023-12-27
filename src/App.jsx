66c69f19b191a48ddc68041cace6979b0392cffd
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-12-27 13:30:57 +0100

    working on favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";

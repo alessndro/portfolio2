a7240feaf0badc19d7068ac6ea22a72a00ac79da
Alessandro Degenkamp <alessandrodegenkamp@hotmail.com>
2023-12-27 12:17:18 +0100

    changed the cursor

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,20 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import AnimatedCursor from "react-animated-cursor"
 
 export default function App() {
 
     return (
       <div className='h-full gray-bg'>
+        <AnimatedCursor 
+          innerSize={8}
+          outerSize={8}
+          color='1, 1, 1'
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={5}
+        />
         <Hero />
         <About />
         <Experience />
